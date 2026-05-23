64e0c65c236be6151d4ca5400d0fbd3ae78333d5
Gabriella Franco <gabriella-nena@hotmail.com>
2026-05-23 20:16:03 -0300

    Commit inicial

---
+++ src/test/java/com/enterprise/raizesnordeste/RaizesnordesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.raizesnordeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesnordesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
